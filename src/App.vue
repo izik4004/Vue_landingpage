6999f6ef1520c1c90b663053fbcebf4deff0af06
izik4004 <izik4004@gmail.com>
2021-03-04 14:00:26 +0100

    added nav-bar

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+      <Nav />
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Nav
+  }
+}
+</script>
+
+<style src="./assets/index.css"/>
